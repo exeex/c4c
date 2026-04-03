faa3f9e7898d2efb71253d388a769c9153fc8dcd
tim.wu <xray0h@gmail.com>
2026-04-03 22:30:25 +0000

    Add u8 deeper predecessor post-add-sub BIR route test

---
+++ tests/c/internal/backend_route_case/two_param_u8_select_eq_split_predecessor_deeper_affine_post_add_sub.c
@@ -0,0 +1,4 @@
+unsigned char choose2_deeper_both_post_chain_u(unsigned char x,
+                                               unsigned char y) {
+  return (unsigned char)((x == y ? x + 8 - 3 + 5 : y + 11 - 4 + 7) + 6 - 2);
+}
