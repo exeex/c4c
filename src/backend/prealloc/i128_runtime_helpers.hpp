3aaca9a315ec26f25e751f4f619df9c18845b48e
tim.wu <xray0h@gmail.com>
2026-05-17 16:14:32 +0000

    Extract i128 runtime helper publisher

---
+++ src/backend/prealloc/i128_runtime_helpers.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "module.hpp"
+
+namespace c4c::backend::prepare {
+
+void populate_i128_runtime_helper_lanes(PreparedBirModule& prepared);
+
+}  // namespace c4c::backend::prepare
